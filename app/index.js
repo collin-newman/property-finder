416fa9f4cf5bc020ae2fafc07d7beaf81c8fbfd0
collin-newman <collin.newman37@gmail.com>
2023-03-31 17:44:42 -0400

    feat: add top level routes

--- app/index.js
+++ app/index.js
@@ -1,67 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, TextInput, Button } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { Link } from "expo-router";
-import { useState } from "react";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
     alignItems: "center",
     marginTop: 50,
   },
-  textInput: {
-    width: "90%",
-    height: 40,
-    backgroundColor: "#fff",
-    borderRadius: 5,
-    marginTop: 10,
-    color: "#000",
-  },
-  searchButton: {
-    width: "90%",
-    height: 40,
-    backgroundColor: "#fff",
-    borderRadius: 5,
-    marginTop: 30,
-    color: "#000",
+  menuItem: {
+    color: "black",
+    fontSize: 20,
+    margin: 20,
   },
 });
 
 export default function App() {
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [state, setState] = useState("");
-  const [zip, setZip] = useState("");
-
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.textInput}
-        value={address}
-        onChange={(e) => {
-          setAddress(e.nativeEvent.text);
-        }}
-      />
-      <TextInput
-        style={styles.textInput}
-        value={city}
-        onChange={(e) => setCity(e.nativeEvent.text)}
-      />
-      <TextInput
-        style={styles.textInput}
-        value={state}
-        onChange={(e) => setState(e.nativeEvent.text)}
-      />
-      <TextInput
-        style={styles.textInput}
-        value={zip}
-        onChange={(e) => setZip(e.nativeEvent.text)}
-      />
-      <Link href={`/property-details/${address}-${city}-${state}-${zip}`}>
-        <Button title="Search" style={styles.searchButton} />
+      <Link style={styles.menuItem} href="/property-search">
+        <Text>Property Search</Text>
       </Link>
-      <StatusBar style="auto" />
+      <Link style={styles.menuItem} href="/map">
+        <Text>Map</Text>
+      </Link>
+      <Link style={styles.menuItem} href="/properties-list">
+        <Text>Properties List</Text>
+      </Link>
     </View>
   );
 }
